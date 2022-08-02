84b48a3dbc6308937d6f4c7a3b29dc41ce3cf2c9
Vladislav Putyrskiy <V.V.Putyrskiy@gmail.com>
2022-08-02 14:26:48 +0300

    Добавьте файлы проекта.

---
+++ 18.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+int n;
+int k;
+
+int rabbit_stairjumper(int n, int k)
+{
+    int c = 0;
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+    for (int i = 1; i <= k; i++)
+    {
+      c+=rabbit_stairjumper(n - i, k);
+    }
+    return c;
+}
+
+int main()
+{   
+    std::cout << "input the number of stairs";
+    std::cin >> n;
+    std::cout << " and the number of them in 1 jump";
+    std::cin >> k;
+    std::cout << rabbit_stairjumper(n, k);
+}
